87af3b740b0530b1188f827edb13c94a32898826
LazyArse <LazyArse>
2026-07-24 09:35:24 +0100

    feat: initial stab at replace/replaceAll, with next/prev search, edit menu + previous term stored

--- src/FindDialog.h
+++ src/FindDialog.h
@@ -4,6 +4,8 @@
 #include <QDialog>
 #include <QLineEdit>
 #include <QCheckBox>
+#include <QPushButton>
+#include <QTextDocument>
 
 class FindDialog : public QDialog
 {
@@ -13,11 +15,31 @@ public:
     explicit FindDialog(QWidget *parent = nullptr);
 
     QString searchTerm() const;
+    QString replaceTerm() const;
     bool regexEnabled() const;
     bool caseSensitive() const;
 
+    void focusSearchInput();
+    void focusReplaceInput();
+
+signals:
+    void findNextRequested(const QString &text, bool useRegex, bool caseSensitive);
+    void findPrevRequested(const QString &text, bool useRegex, bool caseSensitive);
+    void replaceRequested(const QString &search, const QString &replacement, bool useRegex, bool caseSensitive);
+    void replaceAllRequested(const QString &search, const QString &replacement, bool useRegex, bool caseSensitive);
+
+private slots:
+    void emitFindNext();
+    void emitFindPrev();
+    void emitReplace();
+    void emitReplaceAll();
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
+
 private:
     QLineEdit *m_searchInput;
+    QLineEdit *m_replaceInput;
     QCheckBox *m_regexCheck;
     QCheckBox *m_caseCheck;
 };
